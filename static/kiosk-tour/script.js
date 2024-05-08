76baf93197a4bcaaa68be678c075ffa47f4c97bd
Jakob <jakobdanel@aol.com>
2024-05-08 15:36:16 +0200

    Chnaged base layer

--- static/kiosk-tour/script.js
+++ static/kiosk-tour/script.js
@@ -4,14 +4,11 @@ var mapCenter = [51.9619, 7.6257];
 // Create the map
 var map = L.map('map').setView(mapCenter, 13);
 
-var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
-	minZoom: 0,
-	maxZoom: 20,
-	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	ext: 'png'
-});
-
-Stadia_AlidadeSmoothDark.addTo(map);
+// Add a base layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+}).addTo(map);
 
 // Function to add markers to the map
 function addMarkers(kiosks) {
